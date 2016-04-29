87ba7365f9e927378b8fe7d31961a3a3281f40c6
ThunderTeasers <wowgrack@gmail.com>
2016-04-29 18:10:08 +0300

    Complete article Semantic Annotations

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -6,9 +6,13 @@
 
 @section('content')
     <div class="clear" itemscope itemtype="http://schema.org/BlogPosting">
+        <meta itemprop="inLanguage" content="ru-RU"/>
         <div class="page-header">
             <span class="h1" itemprop="headline">{{ $article->title }}</span>
-            <span class="date">{{ \Jenssegers\Date\Date::parse($article->created_at)->format('j F Y') }}</span>
+            <span class="date">
+                <meta itemprop="datePublished" content="{{ \Jenssegers\Date\Date::parse($article->created_at)->format('Y-m-d') }}"/>
+                {{ \Jenssegers\Date\Date::parse($article->created_at)->format('j F Y') }}
+            </span>
         </div>
         <div class="page-description">
             <div itemprop="description">
