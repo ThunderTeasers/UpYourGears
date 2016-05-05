c605e9f43890b10d50a06d5dce8a24bdc0d165fe
ThunderTeasers <wowgrack@gmail.com>
2016-05-06 00:29:29 +0300

    Fix slug in tags

--- resources/views/tag.blade.php
+++ resources/views/tag.blade.php
@@ -8,7 +8,7 @@
     @foreach($articles as $article)
         <div class="category_article clear" itemprop="blogPost" itemscope itemtype="http://schema.org/BlogPosting">
             <span class="page-header h2">
-                <a href="/{{ \App\Models\Category::where('id', $article->category()->first()->parent_id)->select(['id', 'slug'])->first()->slug.'/'.$article->category()->first()->slug.'/'.$article->slug }}" itemprop="name">{{ $article->title }}</a>
+                <a href="{{ $article->slug }}" itemprop="name">{{ $article->title }}</a>
                 <meta itemprop="datePublished" content="{{ \Jenssegers\Date\Date::parse($article->created_at)->format('Y-m-d') }}"/>
                 <span class="date">{{ \Jenssegers\Date\Date::parse($article->created_at)->format('j F Y') }}</span>
             </span>
