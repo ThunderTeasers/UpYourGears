38079933b978d97f7f0f8d6ba420224ea5519adf
ThunderTeasers <wowgrack@gmail.com>
2016-05-21 13:21:27 +0300

    Test breadcrumbs microdata

--- resources/views/includes/breadcrumbs.blade.php
+++ resources/views/includes/breadcrumbs.blade.php
@@ -1,13 +1,19 @@
 @if(isset($breadcrumbs))
     {!! $breadcrumbs->generate() !!}
 @else
-    <ul class='breadcrumbs clear'>
-        <li>
-            <a href='/'>Главная</a>
+    <ul class='breadcrumbs clear' itemscope itemtype="http://schema.org/BreadcrumbList">
+        <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
+            <a itemprop="item" href="/">
+                <span itemprop="name">Главная</span>
+                <meta itemprop="position" content="1">
+            </a>
             <span class='divider'>&gt;</span>
         </li>
-        <li>
-            <span>404</span>
+        <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
+            <span itemprop="item">
+                <span itemprop="name">404</span>
+                <meta itemprop="position" content="2">
+            </span>
         </li>
     </ul>
 @endif
