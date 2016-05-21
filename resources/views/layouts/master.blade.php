684fc3e958e941c316a00cf501808078300deacc
ThunderTeasers <wowgrack@gmail.com>
2016-05-21 18:21:12 +0300

    Add counters

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,6 +10,23 @@
     <link rel='shortcut icon' type='image/x-icon' href='/favicon.ico' />
 </head>
 <body>
+<!--LiveInternet counter-->
+<script type="text/javascript"><!--
+    new Image().src = "//counter.yadro.ru/hit?r" +
+            escape(document.referrer) + ((typeof(screen) == "undefined") ? "" :
+            ";s" + screen.width + "*" + screen.height + "*" + (screen.colorDepth ?
+                    screen.colorDepth : screen.pixelDepth)) + ";u" + escape(document.URL) +
+            ";" + Math.random();//--></script><!--/LiveInternet-->
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-71898913-2', 'auto');
+    ga('send', 'pageview');
+</script>
+
     @if(!\Illuminate\Support\Facades\App::isLocal())
         {{ URL::forceSchema('https') }}
     @endif
