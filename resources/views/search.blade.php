a64f3830f09ae799f6a65cbffa79dff23d552f33
ThunderTeasers <wowgrack@gmail.com>
2016-05-21 16:33:31 +0300

    Fix search articles path

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -3,11 +3,11 @@
 @section('content')
     @if(count($articles) > 0)
         @foreach($articles as $article)
-            <div class="clear">
-			<span class="h3">
-				<a href="/articles/{{ $article->category()->first()->slug }}/{{ $article->slug }}">{{ $article->title }}</a>
-			</span>
-                <div class="page-description">
+            <div class="category_article clear" itemprop="blogPost" itemscope itemtype="http://schema.org/BlogPosting">
+                <span class="page-header h2">
+                    <a href="{{ $article->category()->first()->parent()->first() ? '/'.$article->category()->first()->parent()->first()->slug : '' }}/{{ $article->category()->first()->slug }}/{{ $article->slug }}">{{ $article->title }}</a>
+                </span>
+                <div class="page-description" itemprop="description">
                     {!! $article->description !!}
                 </div>
             </div>
