23dedfef99868578fe5a1fc9bb37909dddca37f8
ThunderTeasers <wowgrack@gmail.com>
2016-05-26 22:06:19 +0300

    Add microdata to about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -11,7 +11,11 @@
 @endsection
 
 @section('content')
-    <h1>Контакты</h1>
-    <p>Сайт создан для сбора полезной информации в одном месте, чтобы любой человек будь то новичок или более опытный смог найти для себя что-то полезное и интересное.</p>
-    <p>Связаться с администратором можно по email: <a href="mailto:admin@upyourgears.ru">admin@upyourgears.ru</a></p>
+    <div itemscope itemtype="http://schema.org/Blog">
+        <h1 itemprop="name">Контакты</h1>
+        <div itemprop="description">
+            <p>Сайт создан для сбора полезной информации в одном месте, чтобы любой человек будь то новичок или более опытный смог найти для себя что-то полезное и интересное.</p>
+            <p>Связаться с администратором можно по email: <a href="mailto:admin@upyourgears.ru">admin@upyourgears.ru</a></p>
+        </div>
+    </div>
 @endsection
